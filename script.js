03d0808b24029470f3f106219f097f283bea90f2
dkimd5 <dkimd5@mail.ru>
2021-02-01 14:02:37 +0300

    temperature done, name done

---
+++ script.js
@@ -0,0 +1,7 @@
+var temperC = prompt("Температура по Цельсию");
+var temperF = (9 / 5) * temperC + 32;
+alert('Тепмпература по Фаренгейту - ' + temperF);
+
+var name = "Василий";
+var admin = name;
+alert("admin = " + admin);
